e4405c4ddc5a727dac6eafd2e7ea897e9ad34bb2
2777negatyw <przemek.telefin@icloud.com>
2026-04-27 21:44:08 +0200

    Add files via upload
    Learning:  while,if.

---
+++ Python_2_while/main.py
@@ -0,0 +1,6 @@
+weather = str(input("What is the weather like today? "))
+while weather == "sunny":
+    print ("Go outside and enjoy the sun!")
+    break
+else:
+    print ("I don't know what the weather is like, but I hope you have a good day!")
---
+++ Python_3_if/main.py
@@ -0,0 +1,9 @@
+weather = str(input("What is the weather like today? "))
+if weather == "Sunny" or weather == "sunny":
+    print ("Go outside and enjoy the sun!")
+elif weather == "Rainy" or weather == "Cloudy":
+    print ("Take an umbrella with you just in case.")
+elif weather == "Snowy" or weather == "Windy":
+    print ("Wear a warm jacket.")
+else:
+    print ("I don't know what the weather is like, but I hope you have a good day!")
